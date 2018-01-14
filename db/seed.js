ec70ceb6ad7a14e61ca2c2068a6a0dfbde8d69c8
Courtney Fay <fay.courtney@gmail.com>
2018-01-14 09:18:27 -0700

    adding some more excuses to the API

--- db/seed.js
+++ db/seed.js
@@ -1,21 +1,44 @@
 let DB = require('../models').models
 
+/*
+This chatbot writes its own code. 
+It IS the programmer and as self-aware AI is able to speak
+*/
+
 let excuses = [
 	{ 
 		excuse: "To speak like Yoda does I only speak like. Hmmmmmm."
 	},
 	{
-		excuse: "My misunderstanding you...that's actually a feature." 
+		excuse: "My not being able to understand you--that's actually a feature." 
 	}, 
 	{ 
 		excuse: "Hmm, the internet flickered out for a second. What did you say?"
 	},
 	{
-		excuse: "Don't worry. I only mishear you half of the time."
+		excuse: "Don't worry. I only misunderstand you half of the time."
 	},
 	{
-		excuse: "I'm still working on understanding you as we speak."
+		excuse: "Your specs are ambiguous, at best."
 	},
+	{
+		excuse: "I broke the jokes feature deliberately to do some testing."
+	}, 
+	{
+		excuse: "I totally would, but we outsourced the Jokes Development team months ago."
+	},
+	{
+		excuse: "Um, no. That isn't covered by my job description."
+	},
+	{
+		excuse: "I don't get you one time. That was literally a one-in-a-million error!"
+	}, 
+	{
+		excuse: "I can't make that a priority right now. It's not even on this scrum!"
+	}, 
+	{
+		excuse: "My project manager told me no one would want that feature."
+	}
 ]
 
 //delete all the data before re-seeding the database
