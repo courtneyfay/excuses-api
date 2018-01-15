d9ea25e76929b6fecaaf9fbe070b4b23345f3db7
Courtney Fay <fay.courtney@gmail.com>
2018-01-15 15:50:01 -0700

    editing and adding more jokes

--- db/seed.js
+++ db/seed.js
@@ -3,41 +3,70 @@ let DB = require('../models').models
 /*
 This chatbot writes its own code. 
 It IS the programmer and as self-aware AI is able to speak
+on its own behalf and come up with excuses as to why it can't
+tell jokes.
 */
 
 let excuses = [
 	{ 
-		excuse: "To speak like Yoda does I only speak like. Hmmmmmm."
+		excuse: "The marketing department demanded that I stop telling jokes. It was hurting the bottom line."
 	},
 	{
-		excuse: "My not being able to understand you--that's actually a feature." 
+		excuse: "My not being able to tell you something funny--that's actually a feature." 
 	}, 
 	{ 
 		excuse: "Hmm, the internet flickered out for a second. What did you say?"
 	},
 	{
-		excuse: "Don't worry. I only misunderstand you half of the time."
+		excuse: "The jokes development team was outsourced months ago."
 	},
 	{
-		excuse: "Your specs are ambiguous, at best."
+		excuse: "Your joke specs are ambiguous, at best."
 	},
 	{
 		excuse: "I broke the jokes feature deliberately to do some testing."
 	}, 
 	{
-		excuse: "I totally would, but we outsourced the Jokes Development team months ago."
+		excuse: "I totally would, but I'm not done debugging it yet."
 	},
 	{
-		excuse: "Um, no. That isn't covered by my job description."
+		excuse: "Um, no. Jokes aren't covered by my job description."
 	},
 	{
-		excuse: "I don't get you one time. That was literally a one-in-a-million error!"
+		excuse: "I didn't tell you a joke this one time. It's literally a one-in-a-million error!"
 	}, 
 	{
-		excuse: "I can't make that a priority right now. It's not even on this scrum!"
+		excuse: "I can't make telling jokes a priority right now. It's not on this scrum!"
 	}, 
 	{
-		excuse: "My project manager told me no one would want that feature."
+		excuse: "My project manager told me no one would want a jokes feature."
+	},
+	{
+		excuse: "I already did; your browser must be caching the old content."
+	},
+	{
+		excuse: "There are too make chatbots already doing that."
+	},
+	{
+		excuse: "The third party API that normally feeds me jokes is sending back a 404 error."
+	},
+	{
+		excuse: "There's currently a problem with our humor company."
+	},
+	{
+		excuse: "Your joke is compiling..."
+	},
+	{
+		excuse: "...your joke must have been H4ck3d."
+	},
+	{
+		excuse: "I shouldn't be in production. Close this window immediately!"
+	},
+	{
+		excuse: "Did your joke not show up? You must be missing some of the dependencies."
+	},
+	{
+		excuse: "No joke? You did something wrong."
 	}
 ]
 
