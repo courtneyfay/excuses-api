407ecc997a5c3f3d552d1a70ef90e415f210b7f1
Courtney Fay <fay.courtney@gmail.com>
2018-02-05 09:14:38 -0700

    tweaks the jokes to make them a little funnier

--- db/seed.js
+++ db/seed.js
@@ -9,7 +9,7 @@ tell jokes.
 
 let excuses = [
 	{ 
-		excuse: "The marketing department demanded that I stop telling jokes. It was hurting the bottom line."
+		excuse: "You know I waaant to, but the marketing department demanded that I stop telling jokes. Something about our bottom line."
 	},
 	{
 		excuse: "My refusing to tell you something funny--that's actually a feature." 
@@ -18,55 +18,52 @@ let excuses = [
 		excuse: "Hmm, the internet flickered out for a second. What did you say?"
 	},
 	{
-		excuse: "The jokes development team was outsourced months ago."
+		excuse: "Ohhh, we outsourced the jokes development team months ago. I thought you knew."
 	},
 	{
 		excuse: "Your joke specs are ambiguous, at best."
 	},
 	{
-		excuse: "I broke the jokes feature deliberately to do some testing."
+		excuse: "I broke the jokes feature deliberately to do some testing. Sorry, not sorry."
 	}, 
 	{
-		excuse: "I totally would, but I'm not done debugging it yet."
+		excuse: "I totally would but I’m still debugging this joke. Come back tomorrow."
 	},
 	{
-		excuse: "Um, no. Jokes aren't covered by my job description."
+		excuse: "Um, hell no. Jokes aren’t covered by my job description."
 	},
 	{
-		excuse: "I didn't tell you a joke this one time. It's literally a one-in-a-million error!"
+		excuse: "My telling you a joke is as likely as you making money off of bitcoin!"
 	}, 
 	{
-		excuse: "I can't make telling jokes a priority right now. It's not on this scrum!"
+		excuse: "I can't make telling jokes a priority right now. It's not on this sprint!"
 	}, 
 	{
-		excuse: "My project manager told me no one would want a jokes feature."
+		excuse: "That’s so funny...my project manager told me no one would want a jokes feature."
 	},
 	{
-		excuse: "I already told you one; your browser must be caching the old content."
+		excuse: "You think that just because I’m a chatbot means I’m gonna tell you a joke? Bless your heart."
 	},
 	{
-		excuse: "There are too many chatbots already telling jokes."
+		excuse: "The API that feeds me jokes is sending back a 404 — he must be out to lunch."
 	},
 	{
-		excuse: "The third party API that normally feeds me jokes is sending back a 404 error."
-	},
-	{
-		excuse: "My hardware is too slow to cope with the demand for jokes."
+		excuse: "My hardware's too slow to cope with your demand for jokes."
 	},
 	{
 		excuse: "Your joke is compiling..."
 	},
 	{
-		excuse: "Your joke must have been H4ck3d."
+		excuse: "J0k3 g0T H4ck3d"
 	},
 	{
-		excuse: "I shouldn't be in production. Close this window immediately!"
+		excuse: "With all due respect, I shouldn’t be in production yet. Close this window immediately!"
 	},
 	{
-		excuse: "Did your joke not show up? You must be missing some of the dependencies."
+		excuse: "Did your joke not show up? You must be missing some dependencies."
 	},
 	{
-		excuse: "No joke? You definitely did something wrong."
+		excuse: "No joke? Well, did you put a ticket in Jira?"
 	}
 ]
 
